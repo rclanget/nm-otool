0dec7aeb61539eddcac4bfc8ec4827c913987dda
rclanget <roger.clanget@live.fr>
2017-02-19 21:49:38 +0200

    init

---
+++ Makefile
@@ -0,0 +1,44 @@
+NAME		=	./ft_otool
+
+CC			=	gcc
+CFLAGS		=	-Werror -Wall -Wextra
+
+SRC_PATH	=	./srcs/otool/
+SRC_NAME	=	otool.c
+SRC			=	$(addprefix $(SRC_PATH),$(SRC_NAME))
+
+OBJ_PATH	=	./obj/
+OBJ_NAME	=	$(SRC_NAME:.c=.o)
+OBJ			=	$(addprefix $(OBJ_PATH),$(OBJ_NAME))
+
+INC_PATH	=	./inc/
+INC			=	$(addprefix -I,$(INC_PATH))
+
+
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	@$(CC) $(LDFLAGS) $(OBJ) -o $@ && \
+		printf " -->> \033[32mCompilation Success: %s\033[0m             \n" "$@"|| \
+		printf " -->> \033[31mCompilation Failed: %s\033[0m              \n" "$@";
+
+$(OBJ_PATH)%.o: $(SRC_PATH)%.c
+	@mkdir -p $(OBJ_PATH)
+	@$(CC) $(CFLAGS) -o $@ -c $< $(INC) && \
+		printf " -->> \033[32mOk\033[0m: %s                       \r" "$@" || \
+		printf " -->> \033[31mKo\033[0m: %s                       \r" "$@";
+
+clean:
+	@rm -rfv $(OBJ_PATH)
+
+fclean: clean
+	@rm -fv $(NAME)
+
+re: fclean all
+
+norme:
+	@norminette $(SRC)
+	@norminette $(INC_PATH)
+
+.PHONY: all, clean, fclean, re, norme
---
+++ srcs/otool/otool.c
@@ -0,0 +1,84 @@
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#define MMAP_FLAG PROT_READ, MAP_PRIVATE
+
+int ft_strlen(const char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		++i;
+	return (i);
+}
+
+int	ft_error(const char *error)
+{
+	write(2, "otool: ", 7);
+	write(2, error, ft_strlen(error));
+	write(2, " error\n", 7);
+	return (EXIT_FAILURE);
+}
+
+int	ft_otool(char *file)
+{
+	int			fd;
+	struct stat	info_file;
+	void		*maped_file;
+
+	if ((fd = open(file, O_RDONLY)) == -1)
+		return (ft_error("open"));
+	if (fstat(fd, &info_file) == -1)
+	{
+		close(fd);
+		return (ft_error("fstat"));
+	}
+	maped_file = mmap(0, info_file.st_size, MMAP_FLAG, fd, 0);
+	close(fd);
+	if (maped_file == MAP_FAILED)
+		return (ft_error("mmap"));
+
+	// Traitement -----
+	printf("%s\n", (char *)maped_file);
+	// ----------------
+
+	if (munmap(maped_file, info_file.st_size) == -1)
+		return (ft_error("munmap"));
+	return (EXIT_SUCCESS);
+}
+
+int	otool(char **files)
+{
+	while (files && *files)
+	{
+		if (ft_otool(*files) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+		++files;
+	}
+	return (EXIT_SUCCESS);
+}
+
+void ft_option(char *arg)
+{
+
+}
+
+int	main(int ac, char **av)
+{
+	int		ret;
+
+	ret = EXIT_FAILURE;
+	ft_option(av);
+	if (ac == 1)
+		ret = ft_otool("a.out");
+	else
+		ret = otool(++av);
+	return (ret);
+}
